8f95d411995ac11b63b5d67db666bc40feef2575
Artur Gutmann <artur.gutmann@mailbox.tu-dresden.de>
2024-01-21 22:05:06 +0100

    python3.9 needs byteorder for int.from_bytes

--- totp.py
+++ totp.py
@@ -7,5 +7,5 @@ def totp(secret, digits = 6, digest = 'sha1'):
 	message = int(time.time() / 30).to_bytes(8, 'big')
 	mac = hmac.digest(key, message, digest)
 	index = mac[-1] & 0b1111
-	value = int.from_bytes(mac[index:index + 4]) & 0x7fffffff
+	value = int.from_bytes(mac[index:index + 4], 'little') & 0x7fffffff
 	return value % (10 ** digits)
